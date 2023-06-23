373eacf9cdad523250d1c87de7e577661c95d3d4
Mihndim <mihndim2016@gmail.com>
2023-06-23 12:28:00 +0100

    Add button props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import Button from "./Button";
 
 function App() {
   return (
     <div className="App">
         <h1>Welcome to React Components</h1>
+        <div>
+         <Button>Click me please</Button>
+        </div>
+        <div>
+         <Button>Buy now !</Button>
+        </div>
+        <div>
+         <Button>See Deal !</Button>
+        </div>
+        <div>
+         <Button>Hide Adds </Button>
+        </div>
+        <div>
+         <Button>Check out ! </Button>
+        </div>
     </div>
   );
 }
