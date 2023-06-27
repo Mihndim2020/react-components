951585bffa307d65dcecd584d3589e1ec4920f01
Mihndim <mihndim2016@gmail.com>
2023-06-27 10:21:20 +0100

    Add accordion items

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import { GoBell, GoDatabase } from 'react-icons/go';
-import Button from "../components/Button";
+import Accordion from "./components/Accordion";
 
 function App() {
-  const handleClick = () => {
-    console.log('I have been clicked !!!');
-  }
+
+  const items = [
+    {
+        label: 'Can I use JS on a project',
+        content: 'Yes you can use Javascript in your project because it is the most popular language in the world'
+    },
+    {
+        label: 'Can I use React on a project',
+        content: 'Yes you can use React in your project because it is the most popular language in the world'
+    },
+    {
+        label: 'Can I use Angular on a project',
+        content: 'Yes you can use Angular in your project because it is the most popular language in the world'
+    }
+]
+
   return (
     <div className="App">
-        <h1>Welcome to React Components</h1>
-        <div>
-         <Button primary rounded outline className="mb-5" onClick={handleClick}> <GoBell /> Click me please</Button>
-        </div>
-        <div>
-         <Button secondary>Buy now !</Button>
-        </div>
-        <div>
-         <Button success> <GoDatabase />See Deal !</Button>
-        </div>
-        <div>
-         <Button warning>Hide Adds </Button>
-        </div>
-        <div>
-         <Button danger rounded>Check out ! </Button>
-        </div>
+        <h1>Welcome to React Accordion</h1>
+        <Accordion items={items}/>
     </div>
   );
 }
