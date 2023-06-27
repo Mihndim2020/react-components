b4ed1ee20bb154f81a977462863ded3da5fa1a5b
Mihndim <mihndim2016@gmail.com>
2023-06-27 08:29:55 +0100

    Add icons

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { GoBell, GoDatabase } from 'react-icons/go';
 import Button from "./Button";
 
 function App() {
@@ -5,13 +6,13 @@ function App() {
     <div className="App">
         <h1>Welcome to React Components</h1>
         <div>
-         <Button primary rounded outline>Click me please</Button>
+         <Button primary rounded outline> <GoBell /> Click me please</Button>
         </div>
         <div>
          <Button secondary>Buy now !</Button>
         </div>
         <div>
-         <Button success>See Deal !</Button>
+         <Button success> <GoDatabase />See Deal !</Button>
         </div>
         <div>
          <Button warning>Hide Adds </Button>
