9c37eba85ce88c02ce030f9e22fba92807d38c83
Mihndim <mihndim2016@gmail.com>
2023-07-05 15:31:14 +0100

    Add sidebar and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Sidebar from './components/Sidebar';
+import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropdownPage';
+import ButtonPage from './pages/ButtonPage';
 
+function App() {
   return (
-    <div>Welcome to my navigation page</div>
-  )
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Routes className="col-span-5">
+        <Route path="/" element={ <Sidebar /> } />
+        <Route path="/accordion" element={ <AccordionPage /> } />
+        <Route path="/dropdown" element={ <DropdownPage /> } />
+        <Route path="/buttons" element={ <ButtonPage /> } />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
+
