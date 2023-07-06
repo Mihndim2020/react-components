ac59394d8ce7578d7cbf85b8d7da2422b1f236ec
Mihndim <mihndim2016@gmail.com>
2023-07-06 10:56:42 +0100

    Fix modal scroll error

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom';
 
 function Modal({ onClose, children, actionBar }) {
+
+  useEffect(() => {
+    document.body.classList.add('overflow-hidden')
+    return () => {      
+      document.body.classList.remove('overflow-hidden') // removes the class when the modal is remove from the screen
+    }
+  }, [])
   return ReactDOM.createPortal(
     <div>
-      <div onClick={onClose} className='absolute inset-0 bg-gray-300 opacity-80'></div>
-      <div className='absolute inset-40 p-10 bg-white'>
+      <div onClick={onClose} className='fixed inset-0 bg-gray-300 opacity-80'></div>
+      <div className='fixed inset-40 p-10 bg-white'>
         <div className='flex flex-col justify-between h-full'>
           {children}
           <div className='flex justify-end'>
